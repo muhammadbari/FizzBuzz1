328b713c1f3a113c8dfc8d671acc719e235624b5
unknown <mbari@sd-tech-12.SimplifyDigital.local>
2014-02-25 11:12:42 +0000

    Fizzbuzz test 1

---
+++ FizzBuzzClass/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzClass
+{
+    public class FizzBuzz
+    {
+        public string GetOutput(int dividend)
+        {
+            if (dividend % 15 == 0)
+                return "fizzbuzz";
+            if (dividend % 5 == 0)
+                return "buzz";
+            if (dividend % 3 == 0)
+                return "fizz";
+            return dividend.ToString();
+        }
+
+        public string GetOutputString(int start, int end)
+        {
+            string output = "";
+            for (int i = start; i <= end; i++)
+            {
+                output += GetOutput(i) + " ";
+            }
+            return output.Trim();
+        }
+    }
+}
